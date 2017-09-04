eba4545748c771fca32016df0a8b49132307dd48
Peter Duke <peter.duke@policygenius.com>
2017-09-04 18:50:53 -0400

    Add user welcome message in navbar

--- app/javascript/packs/Header.jsx
+++ app/javascript/packs/Header.jsx
@@ -1,26 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const Header = () => (
-  <div>
-    <nav className="navbar navbar-default">
-      <div className="navbar-header">
-        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse">
+export default class Header extends React.Component {
+  constructor() {
+    super();
+  }
 
-        </button>
-        <a className="navbar-brand">GroovyMovie</a>
+  componentDidMount() {
+    const { store } = this.context;
+    this.unsubscribe = store.subscribe(() =>
+      this.forceUpdate()
+    );
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    const { store } = this.context;
+    const user = store.getState().user[0];
+
+    return (
+      <div>
+        <nav className="navbar navbar-default">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse"></button>
+            <a className="navbar-brand">GroovyMovie</a>
+          </div>
+          <div className="collapse navbar-collapse">
+            <ul className="nav navbar-nav navbar-left">
+              <li><Link to='/'>Movie Search</Link></li>
+              <li><Link to='/users/sign_up'>Sign Up</Link></li>
+              <li><Link to='/users/sign_in'>Log In</Link></li>
+              <li><Link to='/users/sign_out'>Log Out</Link></li>
+            </ul>
+              { user
+                  ?  <ul className="nav navbar-nav navbar-right"><li><Link to='#'>Welcome back, {user.username}!</Link></li></ul>
+                  :  <ul className="nav navbar-nav navbar-right"><li><Link to='#'>Welcome movie nerds!</Link></li></ul>
+              }
+          </div>
+        </nav>
       </div>
+    )
+  }
+}
 
-      <div className="collapse navbar-collapse">
-        <ul className="nav navbar-nav">
-          <li><Link to='/'>Movie Search</Link></li>
-          <li><Link to='/users/sign_up'>Sign Up</Link></li>
-          <li><Link to='/users/sign_in'>Log In</Link></li>
-          <li><Link to='/users/sign_out'>Log Out</Link></li>
-        </ul>
-      </div>
-    </nav>
-  </div>
-)
+Header.contextTypes = {
+  store: React.PropTypes.object
+};
 
-export default Header;
